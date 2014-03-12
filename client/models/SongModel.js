50058651beb28eac15957758a6031f0c94feaf9b
Hack Reactor Students <uknoiluvu@gmail.com>
2014-03-12 16:56:47 -0700

    succesfully queueing songs

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,6 +1,18 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  initialize: function(params){
+    if(params) {
+      this.set('artist', params.artist);
+      this.set('title', params.title);
+      this.set('url', params.url);
+    }
+  },
+
+  enqueue: function(){
+    this.trigger('enqueue', this);
+  },
+
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
